705eb53efb811a84452db9e15c42a3cd69a2e16c
downpool <downpool.choi@gmail.com>
2023-03-30 16:54:27 +0900

    Initial Commit

---
+++ weatherWithAPI/weatherWithAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherWithAPIApp.swift
+//  weatherWithAPI
+//
+//  Created by 최동현 on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct weatherWithAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
